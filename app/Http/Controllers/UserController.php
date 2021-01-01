3eb2497cf8ed10fa5740d614cb6cc676ce0bab69
David <d.pitarch@outlook.es>
2021-01-01 16:00:54 +0100

    Create Log Model, Migration and Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,10 +11,11 @@ class UserController extends Controller
 {
     // Metodo POST
     // Función para que un usuario con el rol de Administrador pueda dar de alta nuevos empleados
-    public function register (Request $request) {
-        
-        $input = $request -> all ( );
-        $input ['password']  = bcrypt ($input ['password']);
+    public function register(Request $request)
+    {
+
+        $input = $request->all();
+        $input['password']  = bcrypt($input['password']);
 
         // Defino los campos obligatorios para realizar el POST
         $rules = [
@@ -35,56 +36,56 @@ class UserController extends Controller
             'contract' => 'Debes de escoger un tipo de contrato',
         ];
 
-        $validator = validator::make ($input, $rules, $messages);
+        $validator = validator::make($input, $rules, $messages);
 
-        if ($validator -> fails ( )) {
-            return response ( ) -> json ([$validator -> errors ( )], 400);
-        }
-        else {
-            $user = User::create ($input);
+        if ($validator->fails()) {
+            return response()->json([$validator->errors()], 400);
+        } else {
+            $user = User::create($input);
             return $user;
         }
     }
 
     // Función para que un usuario haga Login
-    public function login (Request $request) {
-        
-        $credentials = $request -> only ('email', 'password');
+    public function login(Request $request)
+    {
 
-        if (Auth::attempt ($credentials )) {
-            
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+
             // Devuelvo el usuario a traves del Auth
-            $user = Auth::user ( );
+            $user = Auth::user();
 
             // Asigno la creacion del token al usuario
-            $token = $user -> createToken ('Token') -> accessToken;
+            $token = $user->createToken('Token')->accessToken;
 
             // Establezco la respuesta hacia el Frontend
             $data = [];
-            $data ['user'] = $user;
-            $data ['token'] = $token;
+            $data['user'] = $user;
+            $data['token'] = $token;
 
-            return response ( ) -> json ($data, 200);
-        }
-        else {
-            return response ( ) -> json ([ 'error' => 'Se ha producido un fallo en el proceso, revisa los datos' ], 400);
+            return response()->json($data, 200);
+        } else {
+            return response()->json(['error' => 'Se ha producido un fallo en el proceso, revisa los datos'], 400);
         }
     }
 
-    public function logout (Request $request) {
+    public function logout(Request $request)
+    {
 
-        $token = $request -> user() -> token();
-        $token -> revoke();
+        $token = $request->user()->token();
+        $token->revoke();
 
-        return response () -> json ('Se ha cerrado la sesión de manera correcta', 200);
+        return response()->json('Se ha cerrado la sesión de manera correcta', 200);
     }
 
     // Función para realizar una busqueda concreto de un usuario
-    public function search_one (Request $request) {
+    public function search_one(Request $request)
+    {
+        $input = $request->only('name');
+        $user = User::where($input)->get();
 
-        $input = $request -> only ('name', 'last_name');
-        $user = User::where($input)->get ();
-
-        return response ( ) -> json ($user);
+        return response()->json($user);
     }
 };
---
+++ database/migrations/2021_01_01_144351_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start', 0);
+            $table->dateTime('end', 0);
+            $table->dateTime('pause_start', 0);
+            $table->dateTime('pause_end', 0);
+            $table->ipAddress('ip_direction');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
