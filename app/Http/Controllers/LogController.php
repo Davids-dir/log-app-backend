b2dca850c4cf0b4469f47409a9af40b005376c07
David <d.pitarch@outlook.es>
2021-01-18 01:21:56 +0100

    Fixing show_one function in deployed back

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -85,7 +85,7 @@ class LogController extends Controller
 
     public function show_one(Request $request)
     {
-        $input = $request->input('email');
+        $input = $request->only('email');
 
         $user = User::where($input)->with('logs')->get();
         
