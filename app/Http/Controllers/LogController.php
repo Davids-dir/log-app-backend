f3a652d93ba475ec6f5c31c19625d5264f230778
David <d.pitarch@outlook.es>
2021-01-18 02:08:03 +0100

    Try to fix show_one function

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -85,7 +85,7 @@ class LogController extends Controller
 
     public function show_one(Request $request)
     {
-        $input = $request->input('');
+        $input = $request->input();
         $user = DB::table('users')->where('email', $input)->get();
         
         
